23fb553984bc867a75e6a9a0a3ddcc8c554a4e5c
joyogbu <obyjoy@yahoo.com>
2023-05-22 10:30:16 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 import os
 
@@ -20,6 +20,12 @@ app.register_blueprint(app_views)
 def close(exc):
     """defining the function"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """define the function"""
+    return make_response(jsonify({"error": 'Not found'}), 404)
 
 
 if __name__ == "__main__":
