573a21c20f303b875b3931cac17d1d74556160b7
joyogbu <obyjoy@yahoo.com>
2023-05-22 02:04:06 -0700

    edited task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,14 @@
 
 
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'])
 def statusroute():
     """defining the function"""
-    string = {"status": "ok"}
+    string = {
+               "status": "ok"
+             }
     return jsonify(string)
 
